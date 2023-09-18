7e0e608bd600eeaf8cb2455f480ceaed85754826
Lucas Cota <lucas.a.cota@gmail.com>
2023-09-17 21:40:24 -0300

    generate-seed

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 import { z } from 'zod'
-import { createReadStream } from 'node:fs'
+import { streamToResponse, OpenAIStream} from 'ai'
 import { openai } from "../lib/openai";
 
 export async function generateAICompletionRoute(app: FastifyInstance){
@@ -31,10 +31,19 @@ export async function generateAICompletionRoute(app: FastifyInstance){
           temperature,
           messages: [
             { role: 'user', content: promptMessage}
-          ]
+          ],
+          stream: true,
         })
 
-        return response
+        /* const stream = OpenAIStream(response)
        
+        streamToResponse(stream, reply.raw, {
+          headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+          }
+        }) */
+
+        return response
     })
 }
